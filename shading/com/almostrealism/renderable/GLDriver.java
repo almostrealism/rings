f6acd668bd251d5d59a8ee11956600b157a10172
ashesfall <ashesfall@almostrealism.com>
2018-03-01 12:52:13 -0800

    More OpenGL functionality.

--- shading/com/almostrealism/renderable/GLDriver.java
+++ shading/com/almostrealism/renderable/GLDriver.java
@@ -3,14 +3,19 @@ package com.almostrealism.renderable;
 import com.jogamp.opengl.GL;
 import com.jogamp.opengl.GL2;
 import com.jogamp.opengl.util.gl2.GLUT;
+import org.almostrealism.algebra.Pair;
 import org.almostrealism.algebra.Vector;
 import org.almostrealism.color.RGB;
+
+import java.util.Stack;
 
 public class GLDriver {
 	public static final boolean enableDoublePrecision = false;
 
 	private GL2 gl;
 	private GLUT glut = new GLUT();
+
+	private Stack<Integer> begins;
 
 	public GLDriver(GL2 gl) {
 		this.gl = gl;
@@ -46,17 +51,27 @@ public class GLDriver {
 		}
 	}
 
-	public void clearColorBuffer() {
-		gl.glClear(GL.GL_COLOR_BUFFER_BIT);
+	public void clearColorBuffer() { gl.glClear(GL.GL_COLOR_BUFFER_BIT); }
+
+	public void glQuads() { glBegin(GL2.GL_QUADS); }
+
+	public void glBegin(int code) {
+		gl.glBegin(code);
+		begins.push(code);
 	}
 
-	public void glLoadIdentity() {
-		gl.glLoadIdentity();
+	public void uv(Pair texCoord) {
+		if (enableDoublePrecision) {
+			gl.glTexCoord2d(texCoord.getA(), texCoord.getB());
+		} else {
+			gl.glTexCoord2f((float) texCoord.getA(), (float) texCoord.getB());
+		}
 	}
 
-	public void glFlush() {
-		gl.glFlush();
-	}
+	public void glLoadIdentity() { gl.glLoadIdentity(); }
+
+	public void glFlush() { gl.glFlush(); }
+	public int glEnd() { gl.glEnd(); return begins.pop(); }
 
 	public void glutWireCube(double size) {
 		// TODO  Replace with our own cube code
