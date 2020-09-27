bf99b5f47e5cfaadf2b3e14ff3d6148a173d8177
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-27 16:15:03 -0700

    Enable AcceleratedRankedChoiceProducer use in intersection engine.

--- engine/com/almostrealism/RayIntersectionEngine.java
+++ engine/com/almostrealism/RayIntersectionEngine.java
@@ -32,7 +32,7 @@ import java.util.List;
  * @author  Michael Murray
  */
 public class RayIntersectionEngine implements RayTracer.Engine {
-	public static final boolean enableAcceleratedAggregator = false;
+	public static boolean enableAcceleratedAggregator = true;
 
 	private Scene<ShadableSurface> scene;
 	private ShaderContext sparams;
