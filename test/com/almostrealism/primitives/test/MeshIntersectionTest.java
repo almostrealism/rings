22d78139ed8883ac29f41090b9f73ebce5979c4d
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2020-09-26 23:26:21 -0700

    Improvements to test to use CacheMeshIntersectionKernel.

--- test/com/almostrealism/primitives/test/MeshIntersectionTest.java
+++ test/com/almostrealism/primitives/test/MeshIntersectionTest.java
@@ -7,6 +7,8 @@ import org.almostrealism.algebra.PairBank;
 import org.almostrealism.algebra.ScalarBank;
 import org.almostrealism.algebra.Vector;
 import org.almostrealism.geometry.Ray;
+import org.almostrealism.geometry.RayBank;
+import org.almostrealism.graph.mesh.CachedMeshIntersectionKernel;
 import org.almostrealism.graph.mesh.DefaultVertexData;
 import org.almostrealism.graph.mesh.Mesh;
 import org.almostrealism.graph.mesh.MeshData;
@@ -28,6 +30,8 @@ public class MeshIntersectionTest {
 	private MeshData data;
 	private KernelizedProducer<Ray> ray;
 
+	private int width, height;
+
 	protected Mesh mesh() {
 		DefaultVertexData data = new DefaultVertexData(3, 1);
 		data.getVertices().set(0, new Vector(0.0, 1.0, 0.0));
@@ -47,9 +51,9 @@ public class MeshIntersectionTest {
 		c.setFocus(10.0);
 		c.setLensRadius(0.2);
 
-		int w = 100;
-		int h = (int)(c.getProjectionHeight() * (w / c.getProjectionWidth()));
-		return c.rayAt(new PassThroughProducer<>(0), StaticProducer.of(new Pair(w, h)));
+		width = 100;
+		height = (int)(c.getProjectionHeight() * (width / c.getProjectionWidth()));
+		return c.rayAt(new PassThroughProducer<>(0), StaticProducer.of(new Pair(width, height)));
 	}
 
 	@Before
@@ -61,12 +65,18 @@ public class MeshIntersectionTest {
 
 	@Test
 	public void intersectAt() {
-		PairBank input = new PairBank(2);
+		CachedMeshIntersectionKernel kernel = new CachedMeshIntersectionKernel(data, ray);
+
+		int size = 3;
+
+		PairBank input = new PairBank(size);
 		input.set(0, new Pair(0.00, 0.00));
 		input.set(1, new Pair(43.50, 92.00));
+		input.set(2, new Pair(width / 2, height / 2));
 
-		ScalarBank distances = new ScalarBank(2);
-		data.evaluateIntersectionKernel(ray, distances, new MemoryBank[] { input }, 0, distances.getCount());
+		ScalarBank distances = new ScalarBank(size);
+		kernel.setDimensions(width, height, 1, 1);
+		kernel.kernelEvaluate(distances, new MemoryBank[] { input });
 
 		System.out.println("distance(0) = " + distances.get(0).getValue());
 		Assert.assertEquals(-1.0, distances.get(0).getValue(), Math.pow(10, -10));
@@ -74,14 +84,30 @@ public class MeshIntersectionTest {
 		System.out.println("distance(1) = " + distances.get(1).getValue());
 		Assert.assertEquals(-1.0, distances.get(1).getValue(), Math.pow(10, -10));
 
-		RankedChoiceProducer<Vector> closestNormal = new RankedChoiceProducer<>(Intersection.e, false);
-		closestNormal.add(new ProducerWithRankAdapter<>(
-					StaticProducer.of(data.get(0).getNormal()),
-					new AdaptProducer<>(Triangle.intersectAt, ray, StaticProducer.of(data.get(0)))));
-		closestNormal.evaluate(new Object[] { new Pair(43.50, 92.00) });
+		System.out.println("distance(2) = " + distances.get(2).getValue());
+		Assert.assertEquals(1.0, distances.get(2).getValue(), Math.pow(10, -10));
+
+		Producer<Vector> closestNormal = kernel.getClosestNormal();
+		Vector n = closestNormal.evaluate(new Object[] { input.get(2) });
+		System.out.println("normal(2) = " + n);
 	}
 
-	public void intersectAtGrid() {
+	@Test
+	public void triangleIntersectAt() {
+		RayBank in = new RayBank(1);
+		ScalarBank distances = new ScalarBank(1);
 
+		in.set(0, 0.0, 0.0, 1.0, 0.0, 0.0, -1.0);
+		Triangle.intersectAt.kernelEvaluate(distances, new MemoryBank[] { in, data });
+		System.out.println("distance = " + distances.get(0).getValue());
+		Assert.assertEquals(1.0, distances.get(0).getValue(), Math.pow(10, -10));
+
+		PairBank out = new PairBank(1);
+		PairBank conf = new PairBank(1);
+		conf.set(0, new Pair(1, Intersection.e));
+		RankedChoiceProducer.highestRank.kernelEvaluate(out, new MemoryBank[] { distances, conf });
+		System.out.println("highest rank: " + out.get(0));
+		Assert.assertEquals(1.0, out.get(0).getA(), Math.pow(10, -10));
+		Assert.assertEquals(0.0, out.get(0).getB(), Math.pow(10, -10));
 	}
 }
