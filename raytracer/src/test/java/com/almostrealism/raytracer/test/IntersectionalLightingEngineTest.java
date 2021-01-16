5058f682a30a2f1e8d9d990dc12450ff12a2f330
ashesfall@almostrealism.com <ashesfall@almostrealism.com>
2021-01-16 05:43:07 -0800

    Expanded test.

--- raytracer/src/test/java/com/almostrealism/raytracer/test/IntersectionalLightingEngineTest.java
+++ raytracer/src/test/java/com/almostrealism/raytracer/test/IntersectionalLightingEngineTest.java
@@ -4,11 +4,16 @@ import com.almostrealism.lighting.PointLight;
 import com.almostrealism.primitives.Sphere;
 import com.almostrealism.rayshade.DiffuseShader;
 import com.almostrealism.raytrace.IntersectionalLightingEngine;
+import io.almostrealism.code.OperationAdapter;
+import io.almostrealism.relation.Evaluable;
 import io.almostrealism.relation.Producer;
+import org.almostrealism.algebra.Scalar;
 import org.almostrealism.algebra.Vector;
+import org.almostrealism.algebra.computations.DotProduct;
 import org.almostrealism.color.Light;
 import org.almostrealism.color.RGB;
 import org.almostrealism.color.ShaderContext;
+import org.almostrealism.color.computations.GeneratedColorProducer;
 import org.almostrealism.geometry.Curve;
 import org.almostrealism.geometry.Intersectable;
 import org.almostrealism.geometry.Ray;
@@ -30,6 +35,23 @@ public class IntersectionalLightingEngineTest implements CodeFeatures {
 
 		return new IntersectionalLightingEngine(r, (Intersectable) s,
 				new ArrayList<>(), l, new ArrayList<>(), c);
+	}
+
+	protected GeneratedColorProducer generatedColorProducer() {
+		AcceleratedComputationEvaluable<RGB> engine = engine();
+		return (GeneratedColorProducer) ((OperationAdapter) engine.getComputation()).getInputs().get(2);
+	}
+
+	protected DotProduct dotProduct() {
+		return (DotProduct) ((OperationAdapter) generatedColorProducer().getProducer()).getInputs().get(1);
+	}
+
+	@Test
+	public void compileDotProduct() {
+		DotProduct dp = dotProduct();
+		Evaluable<Scalar> ev = dp.get();
+		((OperationAdapter) ev).compile();
+		System.out.println(ev.evaluate());
 	}
 
 	@Test
