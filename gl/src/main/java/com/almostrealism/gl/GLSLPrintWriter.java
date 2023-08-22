07168eeb93efb4c8cd9bf29f6118fa4b566f7a1a
Michael Murray <michael@almostrealism.com>
2023-08-21 18:28:46 -0700

    Updates due to change in Common.

--- gl/src/main/java/com/almostrealism/gl/GLSLPrintWriter.java
+++ gl/src/main/java/com/almostrealism/gl/GLSLPrintWriter.java
@@ -27,10 +27,10 @@ import java.io.OutputStream;
  * a shading language similar to C.
  */
 public class GLSLPrintWriter extends CPrintWriter {
-	public GLSLPrintWriter(OutputStream out) { super(out, null, false); }
+	public GLSLPrintWriter(OutputStream out) { super(out, null); }
 
 	/**
 	 * Constructs a new {@link GLSLPrintWriter} for writing GLSL to the specified {@link PrintWriter}.
 	 */
-	public GLSLPrintWriter(PrintWriter p) { super(p, null, false); }
+	public GLSLPrintWriter(PrintWriter p) { super(p, null); }
 }
