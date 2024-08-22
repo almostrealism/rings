6fb77d7082addab75b012940ed04b7a9b68f551e
Michael Murray <michael@almostrealism.com>
2024-08-21 18:20:41 -0700

    Improvements to FeatureComputer.

--- audio/src/main/java/org/almostrealism/audio/feature/FeatureComputer.java
+++ audio/src/main/java/org/almostrealism/audio/feature/FeatureComputer.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 Michael Murray
+ * Copyright 2024 Michael Murray
  *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package org.almostrealism.audio.feature;
 
+import io.almostrealism.code.ComputeRequirement;
 import io.almostrealism.relation.Producer;
 import org.almostrealism.algebra.Pair;
 import org.almostrealism.algebra.ScalarTable;
@@ -29,6 +30,7 @@ import org.almostrealism.audio.computations.SplitRadixFFT;
 import org.almostrealism.CodeFeatures;
 import org.almostrealism.collect.PackedCollection;
 import org.almostrealism.hardware.Hardware;
+import org.almostrealism.time.computations.FourierTransform;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -37,6 +39,7 @@ import java.util.stream.IntStream;
 
 public class FeatureComputer implements CodeFeatures {
 	public static boolean enableVerbose = false;
+	public static boolean enableGpuFft = false;
 
 	private static final double epsilon = 0.00000001;
 
@@ -45,7 +48,7 @@ public class FeatureComputer implements CodeFeatures {
 
 	private final WaveMath math;
 
-	private final Evaluable<? extends PackedCollection<Pair<?>>> fft;
+	private final Evaluable<? extends PackedCollection<?>> fft;
 
 	private final Evaluable<? extends PackedCollection<Scalar>> processWindow;
 	private Evaluable<? extends PackedCollection<Scalar>> preemphasizeAndWindowFunctionAndPad;
@@ -107,7 +110,9 @@ public class FeatureComputer implements CodeFeatures {
 
 		PackedCollection<Pair<?>> fftOutput = Pair.bank(paddedWindowSize);
 
-		fft = new ComplexFFT(Hardware.getLocalHardware().getComputeContext(), paddedWindowSize, true, v(2 * paddedWindowSize, 0));
+		// fft = new ComplexFFT(Hardware.getLocalHardware().getComputeContext(), paddedWindowSize, true, v(2 * paddedWindowSize, 0));
+		fft = fft(paddedWindowSize, v(shape(paddedWindowSize, 2), 0),
+				enableGpuFft ? ComputeRequirement.GPU : ComputeRequirement.CPU).get();
 
 		int count = settings.getFrameExtractionSettings().getWindowSize();
 		Producer<PackedCollection<Scalar>> processWindow = null;
@@ -207,6 +212,7 @@ public class FeatureComputer implements CodeFeatures {
 								double vtlnWarp,
 								BiConsumer<Integer, PackedCollection<Scalar>> output) {
 		Scalar newSampleFreq = settings.getFrameExtractionSettings().getSampFreq();
+
 		if (sampleFreq.getValue() == newSampleFreq.getValue()) {
 			compute(wave, vtlnWarp, output);
 		} else {
@@ -257,7 +263,7 @@ public class FeatureComputer implements CodeFeatures {
 	private double dot(PackedCollection<Scalar> x, PackedCollection<Scalar> y) {
 		return Math.max(
 				IntStream.range(0, x.getCount()).mapToDouble(i ->
-						x.get(i).x() * y.get(i).x()).sum(), epsilon);
+						x.valueAt(i, 0) * y.valueAt(i, 0)).sum(), epsilon);
 	}
 
 	private double logDot(PackedCollection<Scalar> x, PackedCollection<Scalar> y) {
@@ -328,7 +334,7 @@ public class FeatureComputer implements CodeFeatures {
 
 		long start = System.currentTimeMillis();
 
-		PackedCollection<Pair<?>> signalFrame;
+		PackedCollection<?> signalFrame;
 
 		if (fft != null) {
 			signalFrame = fft.evaluate(toPairBank(realSignalFrame, complexSignalFrame));
@@ -542,7 +548,7 @@ public class FeatureComputer implements CodeFeatures {
 			}
 		}
 
-		window = preemphasizeAndWindowFunctionAndPad.evaluate(window);
+		window = preemphasizeAndWindowFunctionAndPad.evaluate(window.traverse(0));
 
 		if (enableVerbose) System.out.println("--> processWindow: " + (System.currentTimeMillis() - start));
 
