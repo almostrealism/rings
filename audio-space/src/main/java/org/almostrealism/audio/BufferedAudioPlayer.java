dce53b366ca6c7b528add9a5095a00483e4bf6f6
Michael Murray <michael@almostrealism.com>
2024-11-30 15:44:13 -0800

    Updated BufferedAudioPlayer to support play, stop, seek and volume adjustment along with notifying time listeners.

--- audio-space/src/main/java/org/almostrealism/audio/BufferedAudioPlayer.java
+++ audio-space/src/main/java/org/almostrealism/audio/BufferedAudioPlayer.java
@@ -16,32 +16,64 @@
 
 package org.almostrealism.audio;
 
+import org.almostrealism.algebra.Scalar;
 import org.almostrealism.audio.data.WaveData;
 import org.almostrealism.audio.line.BufferedOutputScheduler;
 import org.almostrealism.collect.PackedCollection;
+import org.almostrealism.graph.TimeCell;
 import org.almostrealism.graph.temporal.WaveCell;
-import org.almostrealism.graph.temporal.WaveCellData;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.DoubleConsumer;
 
 public class BufferedAudioPlayer extends AudioPlayerBase implements CellFeatures {
 	private WaveData data;
 	private PackedCollection<?> loopDuration;
+	private List<DoubleConsumer> timeListeners;
 
 	private CellList cells;
+	private TimeCell clock;
+	private Scalar level;
+
+	private double volume;
+	private double duration;
+	private boolean playing;
+
+	private long waitTime;
+	private Thread monitor;
+	private boolean stopped;
 
 	public BufferedAudioPlayer(List<String> channelNames,
 							   int sampleRate, int bufferFrames) {
 		super(channelNames);
 		this.data = new WaveData(new PackedCollection<>(bufferFrames).traverseEach(), sampleRate);
 		this.loopDuration = new PackedCollection<>(1);
+		this.timeListeners = new ArrayList<>();
+		this.volume = 1.0;
 	}
 
-	public void load(String file) {
+	public synchronized void load(String file) {
 		setFileString(file);
 		update();
+
+		if (monitor == null) {
+			monitor = new Thread(() -> {
+				while (!stopped) {
+					try {
+						for (DoubleConsumer listener : timeListeners) {
+							listener.accept(getCurrentTime());
+						}
+						
+						Thread.sleep(waitTime);
+					} catch (Exception e) {
+						warn("Error in scheduled job", e);
+					}
+				}
+			}, "BufferedAudioPlayer Monitor");
+			monitor.start();
+		}
 	}
 
 	public BufferedOutputScheduler deliver(OutputLine out) {
@@ -51,6 +83,11 @@ public class BufferedAudioPlayer extends AudioPlayerBase implements CellFeatures
 
 		if (cells == null) {
 			cells = w(p(loopDuration), data);
+			clock = ((WaveCell) cells.get(0)).getClock();
+			level = ((WaveCell) cells.get(0)).getData().amplitude();
+
+			long bufferDuration = out.getBufferSize() * 1000L / data.getSampleRate();
+			waitTime = bufferDuration / 4;
 		} else {
 			warn("Attempting to deliver to an already active player");
 		}
@@ -72,43 +109,74 @@ public class BufferedAudioPlayer extends AudioPlayerBase implements CellFeatures
 			in.readFrames(result, (int) in.getFramesRemaining());
 
 			int frames = Math.min(result[0].length, this.data.getCollection().getMemLength());
-			double duration = frames / (double) data.sampleRate();
+			duration = frames / (double) data.sampleRate();
 			data.getCollection().setMem(0, result[0], 0, frames);
-			loopDuration.setMem(duration);
 			log("Loaded " + frames + " frames and set duration to " + duration);
+			updateLevel();
 		} catch (IOException e) {
 			warn("Could not load " + getFileString() + " to player", e);
 		}
 	}
 
+	protected void updateLevel() {
+		if (cells == null) return;
+
+		if (playing) {
+			level.setMem(volume);
+			loopDuration.setMem(duration);
+		} else {
+			level.setMem(0.0);
+			loopDuration.setMem(0.0);
+		}
+	}
+
 	@Override
 	public boolean play() {
+		playing = true;
+		updateLevel();
 		return true;
 	}
 
 	@Override
 	public boolean stop() {
-		return false;
+		playing = false;
+		updateLevel();
+		return true;
 	}
 
 	@Override
 	public boolean isPlaying() {
-		return true;
+		return playing;
 	}
 
 	@Override
 	public boolean isReady() {
-		return true;
+		return cells != null;
 	}
 
 	@Override
 	public void setVolume(double volume) {
+		this.volume = volume;
+		updateLevel();
+	}
 
+	protected double getWavePosition() {
+		return clock.getFrame();
+	}
+
+	protected void setWavePosition(double frames) {
+		clock.setFrame(frames);
 	}
 
 	@Override
 	public void seek(double time) {
+		if (cells == null) {
+			return;
+		}
 
+		if (time < 0.0) time = 0.0;
+		if (time > getTotalDuration()) time = getTotalDuration();
+		setWavePosition(time * data.getSampleRate());
 	}
 
 	@Override
@@ -117,9 +185,7 @@ public class BufferedAudioPlayer extends AudioPlayerBase implements CellFeatures
 			return 0;
 		}
 
-		WaveCellData wd = ((WaveCell) cells.get(0)).getData();
-		double frames = wd.waveIndex().toDouble() + wd.wavePosition().toDouble();
-		return frames / data.getSampleRate();
+		return getWavePosition() / data.getSampleRate();
 	}
 
 	@Override
@@ -129,11 +195,13 @@ public class BufferedAudioPlayer extends AudioPlayerBase implements CellFeatures
 
 	@Override
 	public void addTimeListener(DoubleConsumer listener) {
-
+		timeListeners.add(listener);
 	}
 
 	@Override
 	public void destroy() {
+		this.stopped = true;
+		this.monitor = null;
 		this.data.destroy();
 		this.loopDuration.destroy();
 	}
