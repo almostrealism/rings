221977034d9203c31b47da5642e7444ef35ef66c
Michael Murray <michael@almostrealism.com>
2025-12-05 09:34:50 -0800

    Updated use of seed

--- generate/src/main/java/org/almostrealism/ml/audio/AudioGeneratorExample.java
+++ generate/src/main/java/org/almostrealism/ml/audio/AudioGeneratorExample.java
@@ -95,11 +95,12 @@ public class AudioGeneratorExample {
 				generator.setAudioDurationSeconds(8.0);
 
 				for (int i = 0; i < variations; i++) {
+					long s = rand.nextLong();
 					String filename = String.format("%s/pure_gen_%d_seed%d.wav",
-							outputPath, i, seed);
+							outputPath, i, s);
 					System.out.println("Generating variation " +
-							(i + 1) + "/" + variations + " (seed " + seed + ")");
-					generator.generateAudio(prompt, seed, filename);
+							(i + 1) + "/" + variations + " (seed " + s + ")");
+					generator.generateAudio(prompt, s, filename);
 				}
 			}
 		} finally {
